211584061fa9ba7ff6139d2760106b81965eded3
Vidittamrakar21 <221b440@juetguna.in>
2025-05-02 00:22:28 +0530

    Updated Website

--- src/app/Speaker/page.jsx
+++ src/app/Speaker/page.jsx
@@ -32,13 +32,16 @@ function page() {
               <div className="link-list"><a href="https://www.ted.com/tedx/events/62368">View Event Details</a></div>
             </div>
             <div className="speaker-cards">
-              <ProfileCard image="profile.png" name="Someone Interesting" occupation="Coming Soon" />
-              <ProfileCard image="profile.png" name="Someone Interesting" occupation="Coming Soon" />
-              <ProfileCard image="profile.png" name="Someone Interesting" occupation="Coming Soon" />
-              <ProfileCard image="profile.png" name="Someone Interesting" occupation="Coming Soon" />
-              <ProfileCard image="profile.png" name="Someone Interesting" occupation="Coming Soon" />
-              <ProfileCard image="profile.png" name="Someone Interesting" occupation="Coming Soon" />
-              <ProfileCard image="profile.png" name="Someone Interesting" occupation="Coming Soon" />
+            <ProfileCard image="https://res.cloudinary.com/dvonarg5v/image/upload/v1745819377/arpitdwivedi_opgf32.png" name="Arpit Dwivedi" occupation="Science Communicator and Rationalist" />
+            <ProfileCard image="https://res.cloudinary.com/dvonarg5v/image/upload/v1745819419/Farooq_chisty_ujg6fw.png" name="Farooq Chisty" occupation="Entrepreneur" />
+            <ProfileCard image="https://res.cloudinary.com/dvonarg5v/image/upload/v1745819629/gajendrapurohit_y6fuyq.png" name="Dr. Gajendra Purohit" occupation="YouTuber, Mathematics Educator, Professor" />
+            <ProfileCard image="https://res.cloudinary.com/dvonarg5v/image/upload/v1745819588/mohitsharma_fz4hbz.png" name="Mohit Sharma" occupation="Entrepreneur, Founder & CEO at Blacklisted" />
+            <ProfileCard image="https://res.cloudinary.com/dvonarg5v/image/upload/v1745819690/charvi_jain_idukvg.png" name="Charvi Jain" occupation="Psychologist, Entrepreneur" />
+            <ProfileCard image="https://res.cloudinary.com/dvonarg5v/image/upload/v1745819690/babulal_dahiya_n200bc.png" name="Babulal Dahiya" occupation="Farmer, Poet, and Conservationist" />
+            <ProfileCard image="https://res.cloudinary.com/dvonarg5v/image/upload/v1745819504/vrindagupta_zlhnnx.png" name="Vrinda Gupta" occupation="TEDx Speaker, Soft Skills Coach, and Corporate Trainer" />
+            <ProfileCard image="https://res.cloudinary.com/dvonarg5v/image/upload/v1746124493/sunny_gupta_h7gsng.png" name="Sunny Gupta" occupation="Visionary Life Coach, Academic Innovator, and Empowerment Leader" />
+            <ProfileCard image="https://res.cloudinary.com/dvonarg5v/image/upload/v1746124528/BHAWNA_DHARIYA_diohdg.png" name="Bhawna Dhariya" occupation="National-level Athlete, Adventurer" />
+            <ProfileCard image="https://res.cloudinary.com/dvonarg5v/image/upload/v1746124563/dheerajrathi_v0xr30.png" name="Dheeraj Rathi" occupation="Business strategist, financial advisor, and startup mentor" />
             </div>
           </div>
         </div>
